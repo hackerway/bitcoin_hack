8b8e2f7f932b6dec8af47db68a9426b95cd4ca65
Edward Betts <edward@4angle.com>
2014-06-28 16:07:56 +0100

    Add some more fields.

--- model.py
+++ model.py
@@ -1,15 +1,20 @@
-from database import Base, session
-from sqlalchemy import Column, Unicode, ForeignKey, DateTime, Integer, func
-from sqlalchemy.orm import relationship
+from database import Base
+from sqlalchemy import Column, Unicode, ForeignKey, DateTime, Integer, Enum
 
 
 class User(Base):
-    username = Column(Unicode(128), primary_key=True)
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(Unicode(128), nullable=False)
     password = Column(Unicode(128), nullable=False)
     email = Column(Unicode(128), nullable=False)
     first_name = Column(Unicode(128), nullable=False)
     last_name = Column(Unicode(128), nullable=False)
     account_created = Column(DateTime(), nullable=False)
-    last_login = Column(DateTime())
+    last_login = Column(DateTime)
+    account_type = Column(Enum('publisher', 'advertiser'), nullable=False)
 
 
+class Publisher(Base):
+    __tablename__ = 'publisher'
+    id = Column(Integer, ForeignKey('User.id'), primary_key=True)
